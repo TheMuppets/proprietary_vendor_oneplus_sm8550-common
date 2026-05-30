bba08942b51eac8fe441457bbdd4ca9e5ce50d0e
Bruno Martins <bgcngm@gmail.com>
2026-05-30 13:11:46 +0100

    sm8550-common: Update from OOS 11.H.13
    Change-Id: I397a20fd685c8a490f9a2c432f54b81dac995c9b

--- sm8550-common-vendor.mk
+++ sm8550-common-vendor.mk
@@ -1188,7 +1188,7 @@ PRODUCT_PACKAGES += \
     libvasapi \
     vendor.oplus.hardware.communicationcenter-V2-ndk \
     vendor.oplus.hardware.subsys-V5-ndk \
-    vendor.oplus.hardware.subsys_radio-V8-ndk \
+    vendor.oplus.hardware.subsys_radio-V9-ndk \
     vendor.qti.esepowermanager@1.0 \
     vendor.qti.esepowermanager@1.1 \
     odm_lib_rfsa_adsp_aiboost_libQnnHtpV73Skel_so \
